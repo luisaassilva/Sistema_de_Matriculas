254f8b3c55bff02bbe7ccd4615cd416ddf3c0533
Débora <144275722+debora67@users.noreply.github.com>
2025-03-09 19:03:51 -0300

    Update Secretaria.java

--- Codigo/Secretaria.java
+++ Codigo/Secretaria.java
@@ -1,17 +1,20 @@
 package Codigo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Secretaria {
     private String nome;
     private String senha;
+    private List<Disciplina> disciplinas;
 
     public Secretaria(String nome, String senha) {
         this.nome = nome;
         this.senha = senha;
+        this.disciplinas = new ArrayList<>();
     }
 
-      public void listarCursos(List<Curso> cursos) {
+    public void listarCursos(List<Curso> cursos) {
         System.out.println("Cursos:");
         for (Curso curso : cursos) {
             System.out.println(curso.getNome());
@@ -30,5 +33,17 @@ public class Secretaria {
         for (Aluno aluno : alunos) {
             System.out.println(aluno.getNome());
         }
+    }
+
+    public void verificarDisciplinasAtivas() {
+        for (Disciplina disciplina : disciplinas) {
+            if (!disciplina.isAtiva()) {
+                System.out.println("Disciplina " + disciplina.getNome() + " cancelada por falta de alunos.");
+            }
+        }
+    }
+    
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
     }
 }
