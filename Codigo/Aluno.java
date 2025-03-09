a8c55f42ad607941aa490f5e4e5d3d295c4cef35
Débora <144275722+debora67@users.noreply.github.com>
2025-03-09 18:59:15 -0300

    Update Aluno.java

--- Codigo/Aluno.java
+++ Codigo/Aluno.java
@@ -1,35 +1,61 @@
+package Codigo;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Aluno {
     private String nome;
     private String senha;
-    private Integer mensalidade;
+    private int mensalidade;
     private List<Disciplina> disciplinas;
+    private static final int MAX_OBRIGATORIAS = 4;
+    private static final int MAX_OPTATIVAS = 2;
+    private List<Disciplina> disciplinasMatriculadas;
 
-    public Aluno(String nome, String senha, Integer mensalidade) {
+    public Aluno(String nome, String senha, int mensalidade) {
         this.nome = nome;
         this.senha = senha;
         this.mensalidade = mensalidade;
         this.disciplinas = new ArrayList<>();
     }
-    public void setMensalidade(Integer mensalidade) {
-        this.mensalidade = mensalidade;
+
+    public boolean cadastrarDisciplina(Disciplina disciplina, boolean obrigatoria) {
+        long obrigatoriasCount = disciplinas.stream().filter(d -> d instanceof Disciplina).count();
+        if (obrigatoria && obrigatoriasCount >= MAX_OBRIGATORIAS) {
+            return false;
+        }
+        if (!obrigatoria && (disciplinas.size() - obrigatoriasCount) >= MAX_OPTATIVAS) {
+            return false;
+        }
+        disciplinas.add(disciplina);
+        return true;
     }
 
-    public Integer getMensalidade() {
-        return mensalidade;
-    }
-
-    public void cadastrarDisciplina(Disciplina disciplina) {
-        this.disciplinas.add(disciplina);
+    public void cancelarDisciplina(Disciplina disciplina) {
+        disciplinas.remove(disciplina);
     }
 
     public List<Disciplina> getDisciplinas() {
         return disciplinas;
     }
 
+    public void matricularEmDisciplina(Disciplina disciplina) {
+        if (disciplinasMatriculadas.size() < 6) {
+            disciplinasMatriculadas.add(disciplina);
+        }
+    }
+
     public String getNome() {
         return nome;
     }
+    
+    public void setMensalidade(int valor) {
+        this.mensalidade = valor;
+    }
+    
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
+    }
+
+    
 }
