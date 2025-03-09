f593c1eaf98b808a7bef8143f82823288ccf104e
Débora <144275722+debora67@users.noreply.github.com>
2025-03-09 19:01:16 -0300

    Update Disciplina.java

--- Codigo/Disciplina.java
+++ Codigo/Disciplina.java
@@ -1,24 +1,64 @@
 package Codigo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Disciplina {
     private String nome;
     private String id;
-    private Integer valor;
+    private int valor;
     private Professor professor;
+    private List<Aluno> alunosMatriculados;
+    private static final int MIN_ALUNOS = 3;
+    private static final int MAX_ALUNOS = 60;
 
-    public Disciplina(String nome, String id, Integer valor) {
+    public Disciplina(String nome, String id, int valor) {
         this.nome = nome;
         this.id = id;
         this.valor = valor;
-    }
-    public Integer getValor() {
-        return valor;
-    }
-    public void cadastrarProfessor(Professor professor) {
-        this.professor = professor;
+        this.alunosMatriculados = new ArrayList<>();
     }
 
-    public Professor getProfessor() {
-        return professor;
+    public Disciplina(String nome) {
+        this.nome = nome;
+        this.alunosMatriculados = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Aluno> getAlunosMatriculados() {
+        return alunosMatriculados;
+    }
+
+    public boolean matricularAluno(Aluno aluno) {
+        if (alunosMatriculados.size() < MAX_ALUNOS) {
+            alunosMatriculados.add(aluno);
+            return true;
+        }
+        return false;
+    }
+
+    public void cancelarMatricula(Aluno aluno) {
+        alunosMatriculados.remove(aluno);
+    }
+
+    public boolean isAtiva() {
+        return alunosMatriculados.size() >= MIN_ALUNOS;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        if (alunosMatriculados.size() < 60) {
+            alunosMatriculados.add(aluno);
+        }
+    }
+
+    public void removerAluno(Aluno aluno) {
+        alunosMatriculados.remove(aluno);
     }
 }
